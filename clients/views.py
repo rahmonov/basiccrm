c790bc572e3c66da0cc6b6c9005ddfa0887f7505
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-10 21:57:03 +0200

    Use generic views for client views

--- clients/views.py
+++ clients/views.py
@@ -4,55 +4,30 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from clients.forms import ClientForm
 from clients.models import Client
 
 
-class ClientListView(View):
-    def get(self, request):
-        clients = Client.objects.all()
-
-        context = {
-            'clients': clients
-        }
-
-        return render(request, 'clients/list.html', context)
+class ClientListView(ListView):
+    queryset = Client.objects.all()
+    template_name = 'clients/list.html'
+    context_object_name = 'clients'
 
 
-class ClientDetailedView(View):
-    def get(self, request, id):
-        client = Client.objects.get(pk=id)
-
-        context = {
-            'client': client
-        }
-
-        return render(request, 'clients/detailed_view.html', context)
+class ClientDetailedView(DetailView):
+    queryset = Client.objects.all()
+    template_name = 'clients/detailed_view.html'
+    pk_url_kwarg = 'id'
 
 
-class ClientCreateView(View):
-    def get(self, request):
-        form = ClientForm()
+class ClientCreateView(CreateView):
+    form_class = ClientForm
+    template_name = 'clients/create.html'
 
-        context = {
-            'form': form
-        }
-
-        return render(request, 'clients/create.html', context)
-
-    def post(self, request):
-        form = ClientForm(data=request.POST)
-
-        context = {
-            'form': form
-        }
-
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('clients:list'))
-        else:
-            return render(request, 'clients/create.html', context)
+    def get_success_url(self):
+        return reverse('clients:list')
 
 
 class ClientDeleteView(View):
@@ -76,39 +51,11 @@ class ClientDeleteView(View):
         return redirect(reverse('clients:list'))
 
 
-class ClientUpdateView(View):
-    def get(self, request, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            raise Http404()
+class ClientUpdateView(UpdateView):
+    queryset = Client.objects.all()
+    template_name = 'clients/update.html'
+    form_class = ClientForm
+    pk_url_kwarg = 'id'
 
-        form = ClientForm(instance=client)
-
-        context = {
-            'form': form
-        }
-
-        return render(request, 'clients/update.html', context)
-
-    def post(self, request, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            raise Http404()
-
-        form = ClientForm(
-            instance=client,
-            data=request.POST
-        )
-
-        context = {
-            'form': form
-        }
-
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('clients:list'))
-
-        return render(request, 'clients/create.html', context)
-
+    def get_success_url(self):
+        return reverse('clients:list')
