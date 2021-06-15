d42cbe60d7f12ea113c64a5ace741123e20805b5
jahongir <abdurahmonov@gmail.com>
2021-06-15 09:08:49 +0200

    landing functional view changed to generic TeamplateView

--- basiccrm/settings.py
+++ basiccrm/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'basiccrm',
         'USER': 'postgres',
-        'PASSWORD': 'S0meg00dpass',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
