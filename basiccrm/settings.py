5b8823acfe18d1fd97ba240d7d655400e2dd0bc9
jahongir <abdurahmonov@gmail.com>
2021-06-15 08:47:12 +0200

    correct db password added

--- basiccrm/settings.py
+++ basiccrm/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'basiccrm',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'S0meg00dpass',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
