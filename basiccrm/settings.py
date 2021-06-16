54e169162f8044f21149e9eee2c0d2b0246b954e
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2021-06-16 21:19:46 +0500

    database is changed

--- basiccrm/settings.py
+++ basiccrm/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'basiccrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'basiccrm',
+        'USER': 'postgres',
+        'PASSWORD': 'khusan98',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
