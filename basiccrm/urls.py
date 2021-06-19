5e881e0fbad2dc1a3d52e589e3a9d5ad77ae12d3
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-19 11:39:15 +0200

    Add media urls only in localhost

--- basiccrm/urls.py
+++ basiccrm/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('crm/clients/', include('clients.urls'), name='clients'),
     path('users/', include('users.urls'), name='users'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
