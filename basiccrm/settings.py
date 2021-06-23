59870f30233a00804a17ffc80bf0876f5e0dad4a
jahongir <abdurahmonov@gmail.com>
2021-06-23 20:51:17 +0200

    test pr

--- basiccrm/settings.py
+++ basiccrm/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 
+
+# to send test pr
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
