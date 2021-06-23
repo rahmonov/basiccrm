2eb68dbb669e64775eb7de52cdecc86a4e70b537
Fayyozz <fayyozbekk@gmail.com>
2021-06-23 18:18:09 +0500

    added agent delete view

--- agents/urls.py
+++ agents/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from agents.views import AgentListView, AgentCreateView
+from agents.views import AgentListView, AgentCreateView, AgentDeleteView
 
 app_name = 'agents'
 urlpatterns = [
     path('', AgentListView.as_view(), name='list'),
     path('create/', AgentCreateView.as_view(), name='create'),
+    path("<int:id>/delete/", AgentDeleteView.as_view(), name='delete'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.views.generic import ListView
-
+from django.views.generic.edit import DeleteView
 from agents.forms import AgentForm
 from agents.models import Agent
 from users.models import User
@@ -19,23 +19,11 @@ class AgentListView(LoginRequiredMixin, ListView):
             return Agent.objects.filter(business_owner=self.request.user.businessowner)
 
 
-    # def get_queryset(self):
-    #     if self.request.user.is_agent():
-    #         print("Sending agent clients")
-    #         return Client.objects.filter(agent=self.request.user.agent)
-    #     elif self.request.user.is_business_owner():
-    #         print("Sending business owner clients")
-    #         return Client.objects.filter(business_owner=self.request.user.businessowner)
-    #     else:
-    #         print("Sending all clients")
-    #         return Client.objects.all()
-
-
 class AgentCreateView(LoginRequiredMixin, View):
 
     def get(self, request):
         business_owner = request.user.businessowner
-        form = AgentForm(initial={'business_owner':business_owner})
+        form = AgentForm(initial={'business_owner': business_owner})
         form.fields['user'].queryset = User.objects.filter(agent__isnull=True)
         context = {
             'form': form
@@ -59,3 +47,11 @@ class AgentCreateView(LoginRequiredMixin, View):
             return redirect(reverse('agents:list'))
         else:
             return render(request, 'agents/create.html', context)
+
+
+class AgentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Agent
+    context_object_name = "agent"
+    success_url = reverse_lazy('agents:list')
+    template_name = 'agents/delete_confirm.html'
+    pk_url_kwarg = "id"
