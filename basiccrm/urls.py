b7374010e3ae7e48a8b5e2d6df39518286645e65
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-23 20:12:58 +0200

    Add dashboard view

--- basiccrm/urls.py
+++ basiccrm/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from basiccrm.views import LandingView
+from basiccrm.views import LandingView, DashboardView
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('admin/', admin.site.urls),
+    path('crm/', DashboardView.as_view(), name='dashboard'),
     path('crm/clients/', include('clients.urls'), name='clients'),
     path('crm/agents/', include('agents.urls'), name='agents'),
     path('users/', include('users.urls'), name='users'),
--- basiccrm/views.py
+++ basiccrm/views.py
@@ -1,6 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+
+from clients.models import Client
 
 
 class LandingView(TemplateView):
     template_name = 'landing.html'
+
+
+class DashboardView(LoginRequiredMixin, View):
+    def get(self, request):
+        total_client_count = Client.objects.count()
+        unassigned_clients = Client.objects.filter(agent__isnull=True, is_converted=False).count()
+        assigned_clients = Client.objects.filter(agent__isnull=False, is_converted=False).count()
+        converted_clients = Client.objects.filter(agent__isnull=False, is_converted=True).count()
+
+        context = {
+            'total_client_count': total_client_count,
+            'unassigned_clients': unassigned_clients,
+            'assigned_clients': assigned_clients,
+            'converted_clients': converted_clients
+        }
+
+        return render(request, 'dashboard.html', context)
