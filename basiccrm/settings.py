a67089db9ac1c1f5ae882f8e6fc7adfe18e77cdc
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-23 18:52:34 +0200

    Add static_root to settings

--- basiccrm/settings.py
+++ basiccrm/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static-files'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
