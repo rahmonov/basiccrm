492fd287f8125e1c52ebebaae9e5d64546955a28
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-24 22:20:43 +0200

    test prod script

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-ssh root@147.182.128.105 <<EOF
-  cd /root/app/basiccrm
-  git pull
-  source /root/app/basiccrm/venv/bin/activate
-  pip install -r requirements.txt
-  ./manage.py collectstatic --noinput
-  ./manage.py migrate
-  supervisorctl restart basiccrm
-  exit
-EOF
+cd /root/app/basiccrm
+git pull
+source /root/app/basiccrm/venv/bin/activate
+pip install -r requirements.txt
+./manage.py collectstatic --noinput
+./manage.py migrate
+supervisorctl restart basiccrm
