6bb08610d7e18e13c3d726b143382a0c07772e69
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-30 19:39:44 +0200

    Add CRUD APIViews for Client

--- api/views.py
+++ api/views.py
@@ -34,3 +34,41 @@ class ClientDetailAPIView(APIView):
         serializer = ClientSerializer(client)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(instance=client, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(instance=client, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        client.delete()
+
+        return Response(data={'message': 'deleted'}, status=status.HTTP_204_NO_CONTENT)
