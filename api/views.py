84317e3f79488658aee785dc35b2a2530a73157f
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-30 20:06:01 +0200

    Use generic views for Client endpoints

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import status
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,69 +7,86 @@ from clients.models import Client
 from api.serializer import ClientSerializer
 
 
-class ClientListAPIView(APIView):
-    def get(self, request):
-        clients = Client.objects.all()
-        serializer = ClientSerializer(clients, many=True)
-
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ClientSerializer(data=request.data)
-
-        if serializer.is_valid():
-            client = serializer.save()
-
-            return Response(data=ClientSerializer(client).data, status=status.HTTP_201_CREATED)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ClientListAPIView(generics.ListCreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
 
 
-class ClientDetailAPIView(APIView):
-    def get(self, request, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+# class ClientListAPIView(APIView):
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request):
+#         clients = Client.objects.all()
+#         serializer = ClientSerializer(clients, many=True)
+#
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#     def post(self, request):
+#         serializer = ClientSerializer(data=request.data)
+#
+#         if serializer.is_valid():
+#             client = serializer.save()
+#
+#             return Response(data=ClientSerializer(client).data, status=status.HTTP_201_CREATED)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = ClientSerializer(client)
 
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+class ClientDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'id'
 
-    def put(self, request, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = ClientSerializer(instance=client, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = ClientSerializer(instance=client, data=request.data, partial=True)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        client.delete()
-
-        return Response(data={'message': 'deleted'}, status=status.HTTP_204_NO_CONTENT)
+# class ClientDetailAPIView(APIView):
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request, id):
+#         try:
+#             client = Client.objects.get(pk=id)
+#         except Client.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = ClientSerializer(client)
+#
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#     def put(self, request, id):
+#         try:
+#             client = Client.objects.get(pk=id)
+#         except Client.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = ClientSerializer(instance=client, data=request.data)
+#
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def patch(self, request, id):
+#         try:
+#             client = Client.objects.get(pk=id)
+#         except Client.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = ClientSerializer(instance=client, data=request.data, partial=True)
+#
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, id):
+#         try:
+#             client = Client.objects.get(pk=id)
+#         except Client.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         client.delete()
+#
+#         return Response(data={'message': 'deleted'}, status=status.HTTP_204_NO_CONTENT)
